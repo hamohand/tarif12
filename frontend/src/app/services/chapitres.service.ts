892a1e343d0b308e3201db8ad7d9a65dfffb2005
moh <hamohand@gmail.com>
2025-07-23 03:29:24 +0200

    modif ChapitresService: variable chapitresUrl

--- frontend/src/app/services/chapitres.service.ts
+++ frontend/src/app/services/chapitres.service.ts
@@ -1,12 +1,23 @@
 import {Injectable, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChapitresService{
-    private apiUrl = '/api/chapitres'; // pour getAllChapitres
+    /**
+     * Suivre les conventions REST, où les URL pour une ressource
+     * "chapitres" sont formatées comme `/api/students` et `/api/chapitres/{id}`.
+     * @private
+     */
+
+    private readonly apiUrl = environment.apiUrl;
+    // URL de base pour la ressource étudiant
+    private readonly chapitresUrl = `${this.apiUrl}/chapitres`;
+
+    //private apiUrl = '/api/chapitres'; // pour getAllChapitres
 
     constructor(private http: HttpClient) { }
 
@@ -15,6 +26,6 @@ export class ChapitresService{
      * @returns An Observable with the list of all chapitres
      */
     getAllChapitres(): Observable<any[]> {
-        return this.http.get<any[]>(this.apiUrl);
+        return this.http.get<any[]>(this.chapitresUrl);
     }
 }
