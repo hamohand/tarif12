70e6a3d92cdca29ebbc809720e139062159e38e0
moh <hamohand@gmail.com>
2025-07-23 12:13:39 +0200

    liste Positions4

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,10 @@ import { SearchComponent } from './search/search.component';
 import {ChapitresComponent} from "./chapitres/chapitres.component";
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'search', pathMatch: 'full' },
-    { path: 'search', component: SearchComponent },
-    { path: '**', redirectTo: 'search' }
-  //{ path: '', redirectTo: 'chapitres', pathMatch: 'full' },
-  //{ path: 'chapitres', component: ChapitresComponent },
-  //{ path: '**', redirectTo: 'chapitres' }
+    // { path: '', redirectTo: 'search', pathMatch: 'full' },
+    // { path: 'search', component: SearchComponent },
+    // { path: '**', redirectTo: 'search' }
+  { path: '', redirectTo: 'chapitres', pathMatch: 'full' },
+  { path: 'chapitres', component: ChapitresComponent },
+  { path: '**', redirectTo: 'chapitres' }
 ];
